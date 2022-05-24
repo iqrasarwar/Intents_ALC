09708f0c866e185be17c60421037df41bc5c2301
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-05-24 23:48:39 +0500

    change in activity

--- app/src/main/java/com/example/intents_lec01_postmid/MainActivity.java
+++ app/src/main/java/com/example/intents_lec01_postmid/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("acl1","oncreate");
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.button);
         btn1 = findViewById(R.id.button2);
@@ -34,5 +36,39 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected  void onStart()
+    {
+        super.onStart();
+        Log.d("acl1","onstart");
+    }
+
+    @Override
+    protected  void onResume()
+    {
+        super.onResume();
+        Log.d("acl1","onresume");
+    }
+
+
+    @Override
+    protected  void onPause()
+    {
+        super.onPause();
+        Log.d("acl1","onpause");
+    }
+    @Override
+    protected  void onStop()
+    {
+        super.onStop();
+        Log.d("acl1","onSTOP");
+    }
+    @Override
+    protected  void onRestart()
+    {
+        super.onRestart();
+        Log.d("acl1","onrestart");
     }
 }
